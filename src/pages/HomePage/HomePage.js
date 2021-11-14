beea6a8ce143cb2723f84f81318fd37b87a1cc0f
LHwang01 <LawrenceHwang01@gmail.com>
2021-11-14 17:30:18 -0500

    Added Card to HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,7 +4,12 @@ const HomePage = () => {
     return (
         <div>
             <NavBar />
-            Hello home!
+            <div class="card">
+                <div class="container">
+                    <h1>You're in charge.</h1>
+                    <p>All the tools to help you succeed in your addiction journey.</p>
+                </div>
+            </div>
         </div>
     )
 }
