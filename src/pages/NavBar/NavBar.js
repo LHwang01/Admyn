e319d5284d3c4ffeafbf997459c63993c34f2f46
LHwang01 <LawrenceHwang01@gmail.com>
2021-11-28 16:24:20 -0500

    Changed NavBar Alignment

--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (  
         <div className="NavBar">
-            
             <nav>
-                
                 <ul>
-                    <li className="admyn-logo">
-                       <Link to="/"> <a> Admyn </a> </Link>
+                    <li>
+                       <Link to="/"> <a> Admyn </a></Link>
                     </li>
+                </ul>
+                <ul>
                     <li>
                         <a>Learn More</a>
                     </li>
@@ -18,10 +18,8 @@ const NavBar = () => {
                         <a>Premium</a>
                     </li>
                     <li>
-                    <Link to="/login"> <button> Sign in </button></Link> 
+                        <Link to="/login"> <button> Sign in </button></Link> 
                     </li>
-                   
-
                 </ul>
             </nav>
         </div>
