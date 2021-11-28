f391e6244ac0c434070dde9a47c0d593485d2b49
LHwang01 <LawrenceHwang01@gmail.com>
2021-11-28 15:26:28 -0500

    Fixed Top Card Issues

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,11 @@
-import './HomePage.css'
-import NavBar from '../NavBar/NavBar'
+import './HomePage.css';
+import NavBar from '../NavBar/NavBar';
 import MainCard from './MainCard';
 import LeftCard from './LeftCard';
 import RightCard from './RightCard';
 import BottomCard from './BottomCard';
 import BottomCardOverlay from './BottomCardOverlay';
+import Footer from '../Footer/Footer';
 
 const HomePage = () => {
     return (
@@ -19,6 +20,7 @@ const HomePage = () => {
                 <img src={require('../../images/lady.svg').default} width={"60%"} height={"100%"} alt={'Illustration of a woman working.'} />
                 <BottomCard />
                 <BottomCardOverlay />
+                <Footer />
             </div>
         </div>
     )
