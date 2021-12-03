88f6b3b210df8865083ce96e48b11880f606f608
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-03 14:48:16 -0500

    Added Starting Text to LearnMore Page

--- src/pages/LearnMore/LearnMore.js
+++ src/pages/LearnMore/LearnMore.js
@@ -6,6 +6,19 @@ const LearnMore = () => {
         <div>
             <NavBar />
             <h1>Welcome to Admny!</h1>
+            <p>
+                There is always light at the end of the tunnel! We are here to help you recover from any addiction you might be facing.    
+            </p>
+            <p>
+                Addiction recovery is the process of working towards rebuilding your life after being addicted to something. If you have
+                ever felt like something you are habitually doing is negatively harming you, such as harming your physical wellbeing, 
+                negatively impacting your mental happiess, or causing harm to yourself or those around, you are likey dealing with
+                an addiction.
+            </p>
+            <p>
+                Admyn provides you with several scientifically tested and proven recovery techniques, such as a day counter, a journal,
+                and more! 
+            </p>
         </div>
     );
 }
