aec30210a2a519c12cfc490b36238b7cc217867a
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-03 13:37:47 -0500

    Created Day Counter

---
+++ src/pages/Journal/DayCounter/DayCounter.js
@@ -0,0 +1,11 @@
+import './DayCounter.css'
+
+const DayCounter = () => {
+    return (  
+        <div className="DayCounter">
+            <h1>Days Clean</h1>
+        </div>
+    );
+}
+
+export default DayCounter;
