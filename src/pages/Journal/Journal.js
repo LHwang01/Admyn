b81cb371ab1833b64fb74a3ce5bcc4d8a509e292
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-03 13:37:56 -0500

    Changed Journal Component

--- src/pages/Journal/Journal.js
+++ src/pages/Journal/Journal.js
@@ -1,15 +1,12 @@
 import './Journal.css'
 import NavBar from '../NavBar/NavBar';
-import '../../App.css'
 
 const Journal = () => {
     return (
         <div>
             <NavBar />
-            <div className="journal">
-                <p>This is a journal</p>
-            </div>
         </div>
     );
 }
+
 export default Journal;
