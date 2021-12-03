dd2cc5f62bd9299a6accbbab4080773d478ee2a5
dveliyeva <dynveliyeva@gmail.com>
2021-12-03 13:10:25 -0500

    Added Journal Page

---
+++ src/pages/Journal/Journal.js
@@ -0,0 +1,10 @@
+import NavBar from '../NavBar/NavBar';
+import './Journal.css'
+
+const JournalCard = () => {
+    return (
+        <div className="journal-card">
+        </div>
+    );
+}
+export default Journal;
