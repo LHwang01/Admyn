51624d1e0cf5e69076e1aa28aab68cc599605689
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-03 13:54:24 -0500

    Fixed NavBar Issue

--- src/pages/Journal/Journal.js
+++ src/pages/Journal/Journal.js
@@ -1,5 +1,7 @@
-import './Journal.css'
+import './Journal.css';
+import './DayCounter/DayCounter';
 import NavBar from '../NavBar/NavBar';
+import DayCounter from './DayCounter/DayCounter';
 
 const Journal = () => {
     return (
