70632dc604740dfd72fdef902cfdd55a4c255cca
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-08 20:54:15 -0500

    Revert "Added Relapse Button"
    This reverts commit 8ea14bb9c701b038e0cd7b6fc8b1842209067633.

--- src/pages/Journal/DayCounter/DayCounter.js
+++ src/pages/Journal/DayCounter/DayCounter.js
@@ -1,10 +1,8 @@
 import './DayCounter.css'
-import RelapseButton from '../RelapseButton/RelapseButton';
 
 const DayCounter = () => {
     return (  
         <div className="DayCounter">
-            <RelapseButton />
             <h2>Days Clean</h2>
             <h1>90</h1>
             <h2>Keep Going!</h2>
