382b070a96ddfc60a81f58e089ade1f188efffe0
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-12 14:09:53 -0500

    Reformatted PlusButton Syntax

--- src/pages/Journal/DayCounter/PlusButton/PlusButton.js
+++ src/pages/Journal/DayCounter/PlusButton/PlusButton.js
@@ -5,7 +5,11 @@ const PlusButton = (props) => {
 
     return (
         <div className="PlusButton">
-            <img src={require('../../../../images/plus-sign.png').default} onClick={() => props.setCount(props.count + 1)} width={"50%"} height={"100%"} alt={'Plus Sign'} />
+            <img src={require('../../../../images/plus-sign.png').default} 
+            onClick={() => props.setCount(props.count + 1)} 
+            width={"50%"} 
+            height={"100%"} 
+            alt={'Plus Sign'} />
         </div>
     );
 }
