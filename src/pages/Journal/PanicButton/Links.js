81fe9583405e47b3d72b763ef200fdb9d4bb42f7
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-12 10:50:38 -0500

    Added More to the Possible Links

--- src/pages/Journal/PanicButton/Links.js
+++ src/pages/Journal/PanicButton/Links.js
@@ -1,7 +1,14 @@
 let linksArray = [
     'https://www.theatlantic.com/health/archive/2014/02/playing-tetris-can-reduce-urges-to-eat-smoke-drink/284056/',
     'https://i.imgur.com/bbWSvJx.jpg',
-    'https://i.postimg.cc/mk0JvWgy/Difficult-before-Easy.jpg'
+    'https://i.postimg.cc/mk0JvWgy/Difficult-before-Easy.jpg',
+    'https://i.postimg.cc/3R2pHP7F/End-of-Comfort-Zone.png',
+    'https://i.postimg.cc/jqZ2Js8J/Cold-Shower-Benefits.jpg',
+    'https://i.postimg.cc/23RK5pBj/If-You-Can-Dream-It.jpg',
+    'https://www.smartrecovery.org/5-ways-to-deal-with-urges-and-cravings/',
+    'https://www.youtube.com/watch?v=wnHW6o8WMas',
+    'https://i.postimg.cc/c40zPWZT/Work-Hard-in-Silence.jpg',
+    'https://i.postimg.cc/tRtS3cP6/Lose-Sight-of-the-Shore.jpg'
 ]
 
 export function getRandomLink() {
