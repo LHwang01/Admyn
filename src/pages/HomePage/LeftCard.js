208bb465f68aec838fb9c8962d50df0a76edc41b
LHwang01 <LawrenceHwang01@gmail.com>
2021-12-14 13:01:57 -0500

    Heroku Test

--- src/pages/HomePage/LeftCard.js
+++ src/pages/HomePage/LeftCard.js
@@ -5,7 +5,7 @@ const LeftCard = () => {
         <div className="left-card">
             <div className="left-content">
                 <h3>My Journal</h3>
-                <p>Today I'm feeling overwhelmed and I wish I didn't have to do so much today &#127783;
+                <p>Today I'm feeling overwhelmed and wish I didn't have to do so much today &#127783;
                     <br></br> <br></br>On the bright side, I'm very proud of my progress with using social media less. &#128147;
                 </p>
             </div>
